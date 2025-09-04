0883a2d89f1f9a59050f5ba747933655339edb19
Nicholas Shinn <super.nick.gene.shinn@gmail.com>
2025-09-04 15:21:25 -0400

    Separate app logic from server creation

---
+++ super-ngs-backend/app.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import admin from 'firebase-admin';
+import FileSystem from 'fs';
+import { rateLimit } from 'express-rate-limit';
+import ContactRouter from './routes/contact.route.js';
+import TestRouter from './routes/test.route.js';
+
+// Initialize Express app and get the port from environment variables or default to 3000
+const app = express();
+
+// Configures Firebase Admin SDK
+const firebaseConfig = {
+  credential: admin.credential.cert(JSON.parse(FileSystem.readFileSync('./config/serviceAccountKey.json', 'utf8'))),
+  apiKey: process.env.API_KEY,
+  authDomain: "super-ngs.firebaseapp.com",
+  databaseURL: "https://super-ngs-default-rtdb.firebaseio.com",
+  projectId: "super-ngs"
+};
+
+// Gets references to the Firebase app and database
+app.locals.firebase = admin.initializeApp(firebaseConfig);
+app.locals.db = admin.database();
+
+// Rate limiting middleware to limit repeated requests to public APIs and/or endpoints
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Middleware to parse JSON request bodies and set up rate limiting
+app.use(express.json(), limiter);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello, World!" });
+});
+
+// Sets up the routes
+app.use('/contact', ContactRouter);
+app.use("/test", TestRouter);
+
+export default app;
--- super-ngs-backend/server.js
+++ super-ngs-backend/server.js
@@ -1,45 +1,10 @@
-import express from 'express';
-import admin from 'firebase-admin';
-import FileSystem from 'fs';
-import { rateLimit } from 'express-rate-limit';
-import ContactRouter from './routes/contact.route.js';
-import TestRouter from './routes/test.route.js';
+import app from "./app.js";
 
-// Initialize Express app and get the port from environment variables or default to 3000
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configures Firebase Admin SDK
-const firebaseConfig = {
-  credential: admin.credential.cert(JSON.parse(FileSystem.readFileSync('./config/serviceAccountKey.json', 'utf8'))),
-  apiKey: process.env.API_KEY,
-  authDomain: "super-ngs.firebaseapp.com",
-  databaseURL: "https://super-ngs-default-rtdb.firebaseio.com",
-  projectId: "super-ngs"
-};
-
-// Gets references to the Firebase app and database
-app.locals.firebase = admin.initializeApp(firebaseConfig);
-app.locals.db = admin.database();
-
 // Starts the server and listens on the specified port
-app.listen(PORT, async () => {
+const server = app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-// Rate limiting middleware to limit repeated requests to public APIs and/or endpoints
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
-
-// Middleware to parse JSON request bodies and set up rate limiting
-app.use(express.json(), limiter);
-
-// Sets up the routes
-app.use('/contact', ContactRouter);
-app.use("/test", TestRouter);
-
-export default app;
+export default server;
