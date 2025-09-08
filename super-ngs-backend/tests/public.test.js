242c9653ccdad00ce557145cceb062bd2552dfc8
Nicholas Shinn <super.nick.gene.shinn@gmail.com>
2025-09-08 11:21:40 -0400

    Update Public tests to include new routes

--- super-ngs-backend/tests/public.test.js
+++ super-ngs-backend/tests/public.test.js
@@ -11,4 +11,44 @@ describe('GET /public/headshot', () => {
   });
 });
 
+// Tests if the headline route returns data
+describe('GET /public/headline', () => {
+  it('should exist', async () => {
+    const res = await request(server).get('/public/headline');
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
+// Tests if the headline/name route returns data
+describe('GET /public/headline/name', () => {
+  it('should exist', async () => {
+    const res = await request(server).get('/public/headline/name');
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
+// Tests if the headline/title route returns data
+describe('GET /public/headline/title', () => {
+  it('should exist', async () => {
+    const res = await request(server).get('/public/headline/title');
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
+// Tests if the headline/linkedin route returns data
+describe('GET /public/headline/linkedin', () => {
+  it('should exist', async () => {
+    const res = await request(server).get('/public/headline/linkedin');
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
+// Tests if the headline/github route returns data
+describe('GET /public/headline/github', () => {
+  it('should exist', async () => {
+    const res = await request(server).get('/public/headline/github');
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
 afterAll(() => server.close());
