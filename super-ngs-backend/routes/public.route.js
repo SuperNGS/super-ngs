8b6d03ceb6d83c4eaa8ab0ba752584a5af0e493e
Nicholas Shinn <super.nick.gene.shinn@gmail.com>
2025-09-08 11:21:58 -0400

    Update Public with new routes for getting headline data

--- super-ngs-backend/routes/public.route.js
+++ super-ngs-backend/routes/public.route.js
@@ -5,6 +5,12 @@ const router = express.Router(); // Create a router instance
 
 // GET a link to the portfolio headshot image
 router.get('/headshot', PublicController.getHeadshot);
+router.get('/headline', PublicController.getHeadline);
+router.get('/headline/name', PublicController.getHeadlineName);
+router.get('/headline/title', PublicController.getHeadlineTitle);
+router.get('/headline/linkedin', PublicController.getHeadlineLinkedin);
+router.get('/headline/github', PublicController.getHeadlineGithub);
+
 
 // Export the router to be used in other parts of the application
 export default router;
