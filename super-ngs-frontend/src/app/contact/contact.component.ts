c204ce04cddb5eda190a013e68588b38cfa6d217
Nicholas Shinn <super.nick.gene.shinn@gmail.com>
2025-09-09 13:49:44 -0400

    Add basic contact form

--- super-ngs-frontend/src/app/contact/contact.component.ts
+++ super-ngs-frontend/src/app/contact/contact.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatButtonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  contactForm: FormGroup;
+  
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      name: ['', [Validators.required]],
+      phone: ['', [Validators.pattern("^[0-9]{10}$")]],
+      message: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form submitted:', this.contactForm.value);
+    }
+  }
 }
