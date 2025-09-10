291ac393e6bc392d6bfc193113140fe62c8be695
Nicholas Shinn <super.nick.gene.shinn@gmail.com>
2025-09-10 19:32:33 -0400

    Update contact service test to simulate a sendMessage()

--- super-ngs-frontend/src/app/services/contact.service.spec.ts
+++ super-ngs-frontend/src/app/services/contact.service.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ContactService } from './contact.service';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpResponse, provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from '../../environments/environment';
 
 describe('ContactService', () => {
   let service: ContactService;
@@ -25,5 +26,22 @@ describe('ContactService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should send email to the API', () => {
+    const mockForm = {
+      email: "test@example.com",
+      name: "Johnny Test",
+      phone: undefined,
+      message: "post"
+    }
+    const mockResponse = {data: ["Message One", "Message Two"]};
+
+    service.sendEmail(mockForm).subscribe( data => expect(data).toEqual(mockResponse.data));
+
+    const req = httpTestingController.expectOne(environment.API_URL + "/contact");
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual(mockForm);
+    req.flush(mockResponse);
   });
 });
