e2f1b3615b6301ea9b8d26af4fc7d56a020cffa6
Nicholas Shinn <super.nick.gene.shinn@gmail.com>
2025-09-10 21:02:42 -0400

    Add test for valid contact form values

--- super-ngs-frontend/src/app/contact/contact.component.spec.ts
+++ super-ngs-frontend/src/app/contact/contact.component.spec.ts
@@ -31,4 +31,79 @@ describe('ContactComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a correctly structured initial contact form', () => {
+    const emailControl = component.contactForm.get('email');
+    expect(emailControl?.invalid).toBeTrue;
+    expect(emailControl?.hasError('required')).toBeTrue();
+
+    const nameControl = component.contactForm.get('name');
+    expect(nameControl?.invalid).toBeTrue();
+    expect(nameControl?.hasError('required')).toBeTrue();
+
+    const phoneControl = component.contactForm.get('phone');
+    expect(phoneControl?.valid).toBeTrue();
+    
+    const messageControl = component.contactForm.get('message');
+    expect(messageControl?.invalid).toBeTrue();
+    expect(messageControl?.hasError('required')).toBeTrue();
+  });
+
+  it('should accept valid names', () => {
+    const nameControl = component.contactForm.get('name');
+    nameControl?.setValue('test test');
+    fixture.detectChanges();
+    expect(nameControl?.valid).toBeTrue();
+  });
+
+  it('should reject invalid names', () => {
+    const nameControl = component.contactForm.get('name');
+    nameControl?.setValue('1234 #$%^');
+    fixture.detectChanges();
+    expect(nameControl?.valid).toBeFalse();
+    expect(nameControl?.hasError('pattern')).toBeTrue();
+  });
+
+  it('should accept valid emails', () => {
+    const emailControl = component.contactForm.get('email');
+    emailControl?.setValue("test@example.com");
+    fixture.detectChanges();
+    expect(emailControl?.valid).toBeTrue();
+  });
+
+  it('should reject invalid emails', () => {
+    const emailControl = component.contactForm.get('email');
+    emailControl?.setValue("test1234");
+    fixture.detectChanges();
+    expect(emailControl?.valid).toBeFalse();
+    expect(emailControl?.hasError('email')).toBeTrue();
+  });
+
+  it('should accept valid phone numbers', () => {
+    const phoneControl = component.contactForm.get('phone');
+    phoneControl?.setValue('1234567890');
+    fixture.detectChanges();
+    expect(phoneControl?.valid).toBeTrue();
+  });
+
+  it('should reject invalid phone numbers', () => {
+    const phoneControl = component.contactForm.get('phone');
+    phoneControl?.setValue('abcdefghij');
+    fixture.detectChanges();
+    expect(phoneControl?.valid).toBeTrue();
+    expect(phoneControl?.hasError('pattern')).toBeTrue();
+  });
+
+  it('should accept valid messages', () => {
+    const messageControl = component.contactForm.get('message');
+    messageControl?.setValue('Hello, this is a test message');
+    expect(messageControl?.valid).toBeTrue();
+  })
+
+  it('should reject invalid messages', () => {
+    const messageControl = component.contactForm.get('message');
+    messageControl?.setValue('test');
+    expect(messageControl?.valid).toBeFalse();
+    expect(messageControl?.hasError('minLength')).toBeTrue();
+  });
 });
