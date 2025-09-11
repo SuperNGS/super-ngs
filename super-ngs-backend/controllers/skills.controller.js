62045d113f1dae5aac1e84022c890a212a6a6b6c
Nicholas Shinn <super.nick.gene.shinn@gmail.com>
2025-09-11 18:00:15 -0400

    Update backend skills moduole to split skills into soft and technical

--- super-ngs-backend/controllers/skills.controller.js
+++ super-ngs-backend/controllers/skills.controller.js
@@ -3,8 +3,41 @@ import SkillsModel from '../models/skills.model.js';
 export default class SkillsController {
 
     static async getSkills(req, res) {
+        SkillsModel.getSkills(req.app)
+            .then((data) => {
+                return res.status(200).json({
+                    success: true,
+                    data: data
+                });
+            })
+            .catch((err) => { 
+                console.error(err);
+                return res.status(500).json({
+                    success: false,
+                    message: 'Internal Server Error'
+                });
+             });
+    }
+    static async getSoftSkills(req, res) {
         const id = req.query?.id;
-        SkillsModel.getSkills(req.app, id)
+        SkillsModel.getSoftSkills(req.app, id)
+            .then((data) => {
+                return res.status(200).json({
+                    success: true,
+                    data: data
+                });
+            })
+            .catch((err) => { 
+                console.error(err);
+                return res.status(500).json({
+                    success: false,
+                    message: 'Internal Server Error'
+                });
+             });
+    }
+    static async getTechnicalSkills(req, res) {
+        const id = req.query?.id;
+        SkillsModel.getTechnicalSkills(req.app, id)
             .then((data) => {
                 return res.status(200).json({
                     success: true,
--- super-ngs-backend/models/skills.model.js
+++ super-ngs-backend/models/skills.model.js
@@ -1,13 +1,35 @@
 export default class SkillsModel {
-    static async getSkills(app, id) {
-        if(id) {
-            return app.locals.db.ref('/skills').child(`/${id}`).once('value', (snapshot) => {
+    static async getSkills(app) {
+        return app.locals.db.ref('/skills').once('value', (snapshot) => {
+            return snapshot.val();
+        }).catch(err => {
+            throw err;
+        });
+    }
+    static async getSoftSkills(app, id) {
+        if( id != undefined ) {
+            return app.locals.db.ref('/skills').child('/soft').child(`/${id}`).once('value', (snapshot) => {
                 return snapshot.val();
             }).catch(err => {
                 throw err;
             });
         } else {
-            return app.locals.db.ref('/skills').once('value', (snapshot) => {
+            return app.locals.db.ref('/skills').child('/soft').once('value', (snapshot) => {
+                return snapshot.val();
+            }).catch(err => {
+                throw err;
+            });
+        }
+    }
+    static async getTechnicalSkills(app, id) {
+        if( id != undefined ) {
+            return app.locals.db.ref('/skills').child('/technical').child(`/${id}`).once('value', (snapshot) => {
+                return snapshot.val();
+            }).catch(err => {
+                throw err;
+            });
+        } else {
+            return app.locals.db.ref('/skills').child('/technical').once('value', (snapshot) => {
                 return snapshot.val();
             }).catch(err => {
                 throw err;
