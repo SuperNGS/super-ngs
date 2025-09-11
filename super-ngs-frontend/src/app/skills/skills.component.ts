c31d51336d38c35de517b2a575df737b8e12f587
Nicholas Shinn <super.nick.gene.shinn@gmail.com>
2025-09-11 13:00:20 -0400

    Add skills expansion panel logic with dummy data

--- super-ngs-frontend/src/app/skills/skills.component.ts
+++ super-ngs-frontend/src/app/skills/skills.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [MatExpansionModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  skills = [
+    {
+      id: 0,
+      name: "Skill One",
+      description: "This is a description"
+    },
+    {
+      id: 1,
+      name: "Skill Two",
+      description: "This is another description"
+    },
+    {
+      id: 2,
+      name: "Skill Three",
+      description: "This is yet another description"
+    }
+  ]
 }
