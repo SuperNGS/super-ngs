fd9f28b9123da61ff278f15bd3a821aac6c588a3
Nicholas Shinn <super.nick.gene.shinn@gmail.com>
2025-09-12 09:40:13 -0400

    Create and route new Projects component

--- super-ngs-frontend/src/app/app.routes.ts
+++ super-ngs-frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,10 @@ export const routes: Routes = [
     {
         path: "skills",
         component: SkillsComponent
+    },
+    {
+        path: "projects",
+        component: ProjectsComponent
     },
     {
         path: "contact",
