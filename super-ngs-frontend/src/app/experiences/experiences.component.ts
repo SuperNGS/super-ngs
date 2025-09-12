51b60ca8f608224111fce5f1e626fcbcb8af97d2
Nicholas Shinn <super.nick.gene.shinn@gmail.com>
2025-09-12 13:10:23 -0400

    Hook ExperiencesService into ExperiencesComponent

--- super-ngs-frontend/src/app/experiences/experiences.component.ts
+++ super-ngs-frontend/src/app/experiences/experiences.component.ts
@@ -1,53 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { Experiences } from '../classes/experiences';
+import { ExperiencesService } from '../services/experiences.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-experiences',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, DatePipe],
   templateUrl: './experiences.component.html',
-  styleUrl: './experiences.component.scss'
+  styleUrl: './experiences.component.scss',
+  providers: [DatePipe]
 })
-export class ExperiencesComponent {
-  experiences = [
-    {
-      id: 0,
-      company: "Experience One",
-      position: "Test One",
-      points: [
-        "Point One",
-        "Point Two",
-        "Point Three"
-      ],
-      start_date: "08/2024",
-      end_date: "08/2025"
-    },
-    {
-      id: 1,
-      company: "Experience Two",
-      position: "Test Two",
-      points: [
-        "Point One",
-        "Point Two",
-        "Point Three"
-      ],
-      start_date: "07/2020",
-      end_date: "10/2021",
-      image: "rit.jpg"
-    },
-    {
-      id: 2,
-      company: "Experience Three",
-      position: "Test Three",
-      points: [
-        "Point One",
-        "Point Two",
-        "Point Three"
-      ],
-      start_date: "08/2026",
-      end_date: "present",
-      image: "rit.jpg",
-      link: "//rit.edu"
-    }
-  ]
+export class ExperiencesComponent implements OnInit {
+  experiences: Experiences[] = [];
+
+  constructor(private experiencesService: ExperiencesService) {}
+
+  ngOnInit(): void {
+    this.experiencesService.getExperiences().subscribe({
+      next: (res) => {
+        this.experiences = res;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 }
