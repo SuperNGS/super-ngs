63e6068b14e27846abc9a3127fc7b79f82b23a01
Nicholas Shinn <super.nick.gene.shinn@gmail.com>
2025-09-12 11:51:06 -0400

    Add mock data and styling to ExperiencesComponent

--- super-ngs-frontend/src/app/experiences/experiences.component.ts
+++ super-ngs-frontend/src/app/experiences/experiences.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-experiences',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './experiences.component.html',
   styleUrl: './experiences.component.scss'
 })
 export class ExperiencesComponent {
-
+  experiences = [
+    {
+      id: 0,
+      company: "Experience One",
+      position: "Test One",
+      points: [
+        "Point One",
+        "Point Two",
+        "Point Three"
+      ],
+      start_date: "08/2024",
+      end_date: "08/2025"
+    },
+    {
+      id: 1,
+      company: "Experience Two",
+      position: "Test Two",
+      points: [
+        "Point One",
+        "Point Two",
+        "Point Three"
+      ],
+      start_date: "07/2020",
+      end_date: "10/2021",
+      image: "rit.jpg"
+    },
+    {
+      id: 2,
+      company: "Experience Three",
+      position: "Test Three",
+      points: [
+        "Point One",
+        "Point Two",
+        "Point Three"
+      ],
+      start_date: "08/2026",
+      end_date: "present",
+      image: "rit.jpg",
+      link: "//rit.edu"
+    }
+  ]
 }
