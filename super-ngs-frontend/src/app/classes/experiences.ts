9df0c3deade8e5cb3bc41116a1dad8963e1ba452
Nicholas Shinn <super.nick.gene.shinn@gmail.com>
2025-09-12 11:37:01 -0400

    Add new experiences interface

---
+++ super-ngs-frontend/src/app/classes/experiences.ts
@@ -0,0 +1,10 @@
+export interface Experiences {
+    id: number;
+    company: string;
+    position: string;
+    start_date: number;
+    end_date: number;
+    points: string[];
+    image?: string;
+    link?: string;
+}
