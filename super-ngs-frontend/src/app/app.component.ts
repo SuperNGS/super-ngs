d9481bdfea20f761c1f75ba900b9a5f3aa5f0f40
Nicholas Shinn <super.nick.gene.shinn@gmail.com>
2025-09-22 11:36:45 -0400

    Link Notification to app frontend

--- super-ngs-frontend/src/app/app.component.ts
+++ super-ngs-frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { NotificationComponent } from "./notification/notification.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, RouterModule, HeaderComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, RouterModule, HeaderComponent, NotificationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
